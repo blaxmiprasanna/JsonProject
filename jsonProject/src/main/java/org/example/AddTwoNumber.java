78f0ebac929a4fd8a22fb5d5f78fb59e2feb7f8b
lakshmikanthambottysundar <lakshmikantham.bottysundar@gmail.com>
2025-08-07 15:22:15 -0500

    Add two number class is added in to the project

---
+++ jsonProject/src/main/java/org/example/AddTwoNumber.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/*******************************************************************************
+ * Author: Prasanna Kumar
+ * Date: 2023-08-07
+ * Description: This class is used to add two numbers
+ * /******************************************************************************/
+
+
+public class AddTwoNumber {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
